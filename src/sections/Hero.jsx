f4976262ed3232d08294297e9b4547770bb33848
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-06-05 20:57:31 +0600

    in hero section, animation added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,7 @@
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+];
 const Hero = () => {
   return (
     <section id="hero" className="relative overflow-hidden">
@@ -9,7 +13,26 @@ const Hero = () => {
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
           <div className="flex flex-col gap-7">
             <div className="hero-text">
-              <h1>Shaping Designs</h1>
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span
+                        key={word.text}
+                        className="flex items-center md:gap-3 gap-1 pb-2"
+                      >
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
               <h1>into real Projects</h1>
               <h1>that Deliver Results</h1>
             </div>
