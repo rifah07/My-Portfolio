b31e3fc3a474b9f923e79be98d4ad053b017da1e
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-06-07 11:15:19 +0600

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,51 @@
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import { useState, useEffect } from "react";
 
-export default Navbar
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  // track if the user has scrolled down the page
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // cleanup the event listener when the component is unmounted
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
+      <div className="inner">
+        <a href="#hero" className="logo">
+          Rifah Sajida Deya
+        </a>
+
+        <nav className="desktop">
+          <ul>
+            {navLinks.map(({ link, name }) => (
+              <li key={name} className="group">
+                <a href={link}>
+                  <span>{name}</span>
+                  <span className="underline" />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            <span>Contact me</span>
+          </div>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
