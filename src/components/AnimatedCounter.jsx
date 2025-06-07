56d11535bb9c7cc77cf998936535ed8c235419ae
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-06-07 11:06:08 +0600

    animated counter updated

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
+import CountUp from "react-countup";
 
 import { counterItems } from "../constants";
 
@@ -47,6 +48,7 @@ const AnimatedCounter = () => {
           >
             <div className="counter-number text-white-50 text-5xl font-bold mb-2">
               0 {item.suffix}
+              <CountUp suffix={item.suffix} end={item.value} />
             </div>
             <div className="text-white-50 text-lg">{item.label}</div>
           </div>
