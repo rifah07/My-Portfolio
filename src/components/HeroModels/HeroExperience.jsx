d7c63cd024654b9165c88e48307dffd849e87fac
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-06-07 10:16:14 +0600

    snow on room added, hehe

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -3,25 +3,29 @@ import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
 import { Room } from "./Room";
 import HeroLights from "./HeroLights";
+import Particles from "./Particles";
 
 const HeroExperience = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
-     
       <OrbitControls
         enablePan={false}
         enableZoom={!isTablet}
-         enableRotate={true}
+        enableRotate={true}
         maxDistance={20}
         minDistance={5}
         minPolarAngle={Math.PI / 5}
         maxPolarAngle={Math.PI / 2}
       />
       <HeroLights />
-      <group scale={isMobile ? 0.7 : 1} position={[0, -3.5, 0]}
-      rotation={[0, -Math.PI/4, 0]}>
+      <Particles count={100} />
+      <group
+        scale={isMobile ? 0.7 : 1}
+        position={[0, -3.5, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+      >
         <Room />
       </group>
     </Canvas>
