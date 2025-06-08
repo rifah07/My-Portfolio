af544cd0e4f16a9e3a3e65dc1180a91c930f39aa
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-06-09 05:51:17 +0600

    Tech Stack implemented

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -1,7 +1,86 @@
-const TechStack = () => {
-  return (
-    <div>TechStack</div>
-  )
-}
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
-export default TechStack
+import TitleHeader from "../components/TitleHeader";
+import TechIconCardExperience from "../components/Models/tech_logos/TechIconCardExperience";
+import { techStackIcons } from "../constants";
+// import { techStackImgs } from "../constants";
+
+const TechStack = () => {
+  useGSAP(() => {
+    // The animation ease is set to "power2.inOut", which is a slow-in fast-out ease
+    gsap.fromTo(
+      ".tech-card",
+      {
+        // Initial values
+        y: 50, // Move the cards down by 50px
+        opacity: 0,
+      },
+      {
+        // Final values
+        y: 0, // Move the cards back to the top
+        opacity: 1,
+        duration: 1,
+        ease: "power2.inOut", // Ease of the animation
+        stagger: 0.2,
+        scrollTrigger: {
+          trigger: "#skills", // Trigger the animation when the user scrolls to the #skills wrapper
+          start: "top center",
+        },
+      }
+    );
+  });
+
+  return (
+    <div id="skills" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="How I Can Contribute & My Key Skills"
+          sub="🤝 What I Bring to the Table"
+        />
+        <div className="tech-grid">
+
+          {techStackIcons.map((techStackIcon) => (
+            <div
+              key={techStackIcon.name}
+              className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
+            >
+
+              <div className="tech-card-animated-bg" />
+              <div className="tech-card-content">
+              
+                <div className="tech-icon-wrapper">
+                  <TechIconCardExperience model={techStackIcon} />
+                </div>
+  
+                <div className="padding-x w-full">
+                  <p>{techStackIcon.name}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+
+          {/* This is for the img part */}
+          {/* {techStackImgs.map((techStackIcon, index) => (
+            <div
+              key={index}
+              className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
+            >
+              <div className="tech-card-animated-bg" />
+              <div className="tech-card-content">
+                <div className="tech-icon-wrapper">
+                  <img src={techStackIcon.imgPath} alt="" />
+                </div>
+                <div className="padding-x w-full">
+                  <p>{techStackIcon.name}</p>
+                </div>
+              </div>
+            </div>
+          ))} */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TechStack;
